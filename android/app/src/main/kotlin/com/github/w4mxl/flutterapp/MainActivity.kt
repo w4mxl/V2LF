a1587de9a689e44c105973d5a338dc7b82e710a1
魏木林 <weimulin@zhongan.io>
2018-11-05 16:45:52 +0800

    Initial commit by wml

---
+++ android/app/src/main/kotlin/com/github/w4mxl/flutterapp/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.github.w4mxl.flutterapp
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity(): FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
