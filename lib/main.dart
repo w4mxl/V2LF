8a9bf372a73c884bf172fce891469c908b47fabb
wml <mxl1989@gmail.com>
2018-11-21 16:36:43 +0800

    主页添加 Drawer、TabBar、TabBarView

--- lib/main.dart
+++ lib/main.dart
@@ -1,109 +1,152 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Flutter Demo',
-      theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
-        primarySwatch: Colors.orange,
-      ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+      theme: new ThemeData(primarySwatch: Colors.green),
+      home: new DefaultTabController(
+          length: 13,
+          child: new Scaffold(
+            appBar: new AppBar(
+                title: new Text("Explore"),
+                elevation:
+                    defaultTargetPlatform == TargetPlatform.android ? 5.0 : 0.0,
+                bottom: new TabBar(
+                  isScrollable: true,
+                  tabs: choices.map((Choice choice) {
+                    return new Tab(
+                      text: choice.title,
+                    );
+                  }).toList(),
+                )),
+            body: new TabBarView(
+              children: choices.map((Choice choice) {
+                print("init" + choice.title);
+                return new Text(choice.title);
+              }).toList(),
+            ),
+            drawer: new Drawer(
+              child: new Column(
+                children: <Widget>[
+                  new UserAccountsDrawerHeader(
+                    accountName: new Text("w4mxl"),
+                    accountEmail: new Text("mxl1989@gmail.com"),
+                    currentAccountPicture: new CircleAvatar(
+                      backgroundImage: new NetworkImage(
+                          "https://cdn.v2ex.com/gravatar/3896b6baf91ec1933c38f370964647b7?s=73&d=retro"),
+                    ),
+                  ),
+                  new ListTile(
+                    leading: new Icon(Icons.explore),
+                    title: new Text("Explore"),
+                  ),
+                  new ListTile(
+                    leading: new Icon(Icons.apps),
+                    title: new Text("Nodes"),
+                  ),
+                  new ListTile(
+                    leading: new Icon(Icons.notifications),
+                    title: new Text("Notifications"),
+                  ),
+                  new ListTile(
+                    leading: new Icon(Icons.favorite),
+                    title: new Text("Favorites"),
+                  ),
+                  new ListTile(
+                    leading: new Icon(Icons.settings),
+                    title: new Text("Settings"),
+                  ),
+                  new ListTile(
+                    leading: new Icon(Icons.feedback),
+                    title: new Text("Feedback"),
+                  )
+                ],
+              ),
+            ),
+          )),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+/// A material design [TabBar] tab.
+class Choice {
+  const Choice({this.title});
 
   final String title;
-
-  @override
-  _MyHomePageState createState() => new _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+const List<Choice> choices = const <Choice>[
+  const Choice(title: '技术'),
+  const Choice(title: '创意'),
+  const Choice(title: '好玩'),
+  const Choice(title: 'APPLE'),
+  const Choice(title: '酷工作'),
+  const Choice(title: '交易'),
+  const Choice(title: '城市'),
+  const Choice(title: '问与答'),
+  const Choice(title: '最热'),
+  const Choice(title: '全部'),
+  const Choice(title: 'R2'),
+  const Choice(title: '最近'),
+  const Choice(title: '关注'),
+];
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
 
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return new Scaffold(
       appBar: new AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: new Text(widget.title),
+        title: new Text("Explore"),
+        elevation: defaultTargetPlatform == TargetPlatform.android ? 5.0 : 0.0,
       ),
-      body: new Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: new Center(),
+      drawer: new Drawer(
         child: new Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug paint" (press "p" in the console where you ran
-          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
-          // window in IntelliJ) to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            new Text(
-              'You have pushed the button this many times:',
+            new UserAccountsDrawerHeader(
+              accountName: new Text("w4mxl"),
+              accountEmail: new Text("mxl1989@gmail.com"),
+              currentAccountPicture: new CircleAvatar(
+                backgroundImage: new NetworkImage(
+                    "https://cdn.v2ex.com/gravatar/3896b6baf91ec1933c38f370964647b7?s=73&d=retro"),
+              ),
             ),
-            new Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+            new ListTile(
+              leading: new Icon(Icons.explore),
+              title: new Text("Explore"),
             ),
+            new ListTile(
+              leading: new Icon(Icons.apps),
+              title: new Text("Nodes"),
+            ),
+            new ListTile(
+              leading: new Icon(Icons.notifications),
+              title: new Text("Notifications"),
+            ),
+            new ListTile(
+              leading: new Icon(Icons.favorite),
+              title: new Text("Favorites"),
+            ),
+            new ListTile(
+              leading: new Icon(Icons.settings),
+              title: new Text("Settings"),
+            ),
+            new ListTile(
+              leading: new Icon(Icons.feedback),
+              title: new Text("Feedback"),
+            )
           ],
         ),
       ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
