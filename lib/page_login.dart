f46bf4c4b7554ccc435c3ed6cfaa4742f8032615
wml <mxl1989@gmail.com>
2019-01-04 22:53:16 +0800

    prepare login：验证码刷新

--- lib/page_login.dart
+++ lib/page_login.dart
@@ -15,92 +15,14 @@ class LoginPage extends StatefulWidget {
 LoginFormData loginFormData;
 
 class _LoginPageState extends State<LoginPage> {
-  final logo = Image.asset("assets/images/logo_v2lf.png");
-
-  final userName = TextFormField(
-    autofocus: false,
-    decoration: InputDecoration(
-        hintText: 'account',
-        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
-  );
-
-  final password = TextFormField(
-    autofocus: false,
-    obscureText: true,
-    decoration: InputDecoration(
-        hintText: 'password',
-        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
-  );
-
-  final captcha = Row(
-    children: <Widget>[
-      //Image.network("https://www.v2ex.com/_captcha?once=91648"),
-      Flexible(
-          child: TextFormField(
-        autofocus: false,
-        decoration: InputDecoration(
-            hintText: 'captcha',
-            contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-            border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
-      )),
-      Padding(
-          padding: const EdgeInsets.only(left: 8.0),
-          child: loginFormData != null
-              ? Image.memory(
-                  loginFormData.bytes,
-                  width: 145.0,
-                  height: 40.0,
-                )
-              : IconButton(
-                  icon: Icon(Icons.sync),
-                  onPressed: () {
-                    // loadCaptcha();
-                  })),
-    ],
-  );
-
-  final loginButton = Material(
-    clipBehavior: Clip.antiAlias,
-    borderRadius: BorderRadius.circular(30.0),
-    shadowColor: Colors.blueGrey.shade100,
-    elevation: 5.0,
-    child: MaterialButton(
-      minWidth: double.infinity,
-      height: 48.0,
-      onPressed: () {
-        // todo 非空检查-登录判断结果
-      },
-      color: Colors.blueGrey,
-      child: Text('Log In', style: TextStyle(color: Colors.white)),
-    ),
-  );
-
-  final forgotLabel = FlatButton(
-    child: Text(
-      'Forgot password?',
-      style: TextStyle(color: Colors.black54),
-    ),
-    onPressed: () {},
-  );
-
-  final closePage = IconButton(
-      icon: new Icon(
-        Icons.close,
-        size: 20.0,
-      ),
-      onPressed: () {
-        //Navigator.of(context).pop();
-      });
-
   @override
   void initState() {
     super.initState();
-    loadCaptcha();
+    refreshCaptcha();
   }
 
-  Future loadCaptcha() async {
+  // 刷新（首次获取）验证码
+  Future refreshCaptcha() async {
     var formData = await v2exApi.parseLoginForm();
     setState(() {
       loginFormData = formData;
@@ -109,6 +31,83 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
+    final logo = Image.asset("assets/images/logo_v2lf.png");
+
+    final userName = TextFormField(
+      autofocus: false,
+      decoration: InputDecoration(
+          hintText: 'account',
+          contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+    );
+
+    final password = TextFormField(
+      autofocus: false,
+      obscureText: true,
+      decoration: InputDecoration(
+          hintText: 'password',
+          contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+    );
+
+    final captcha = Row(
+      children: <Widget>[
+        Flexible(
+            child: TextFormField(
+          autofocus: false,
+          decoration: InputDecoration(
+              hintText: 'captcha',
+              contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+              border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+        )),
+        Padding(
+            padding: const EdgeInsets.only(left: 8.0),
+            child: loginFormData.bytes.length > 0
+                ? GestureDetector(
+                    child: Image.memory(
+                      loginFormData.bytes,
+                      width: 145.0,
+                      height: 40.0,
+                    ),
+                    onTap: () {
+                      refreshCaptcha();
+                    },
+                  )
+                : IconButton(
+                    icon: Icon(Icons.sync),
+                    onPressed: () {
+                      refreshCaptcha();
+                    })),
+      ],
+    );
+
+    final loginButton = Material(
+      clipBehavior: Clip.antiAlias,
+      borderRadius: BorderRadius.circular(30.0),
+      shadowColor: Colors.blueGrey.shade100,
+      elevation: 5.0,
+      child: MaterialButton(
+        minWidth: double.infinity,
+        height: 48.0,
+        onPressed: () {
+          // 登录
+          // todo 非空检查-登录判断结果
+        },
+        color: Colors.blueGrey,
+        child: Text('Log In', style: TextStyle(color: Colors.white)),
+      ),
+    );
+
+    final forgotLabel = FlatButton(
+      child: Text(
+        'Forgot password?',
+        style: TextStyle(color: Colors.black54),
+      ),
+      onPressed: () {
+        // todo 忘记密码 -> 跳转到重置密码web页面
+      },
+    );
+
     return Scaffold(
         floatingActionButton: FloatingActionButton(
           onPressed: () {
