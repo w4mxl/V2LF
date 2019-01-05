76622d8f881045ccdefc338635a13bb5bf56cd6f
wml <mxl1989@gmail.com>
2019-01-05 23:23:03 +0800

    prepare login

---
+++ lib/bloc/bloc_login.dart
@@ -0,0 +1,44 @@
+import 'dart:async';
+
+import 'package:flutter_app/utils/validators_login.dart';
+import 'package:rxdart/rxdart.dart';
+
+class BlocLogin extends Object with LoginValidators implements BaseBloc {
+  final _accountController = BehaviorSubject<String>();
+  final _passwordController = BehaviorSubject<String>();
+  final _captchaController = BehaviorSubject<String>();
+
+  Function(String) get accountChanged => _accountController.sink.add;
+
+  Function(String) get passwordChanged => _passwordController.sink.add;
+
+  Function(String) get captchaChanged => _captchaController.sink.add;
+
+  //Another way
+  // StreamSink<String> get accountChanged => _accountController.sink;
+  // StreamSink<String> get passwordChanged => _passwordController.sink;
+
+  Stream<String> get account => _accountController.stream.transform(accountValidator);
+
+  Stream<String> get password => _passwordController.stream.transform(passwordValidator);
+
+  Stream<String> get captcha => _captchaController.stream.transform(captchaValidator);
+
+  Stream<bool> get submitCheck =>
+      Observable.combineLatest3(account, password, captcha, (a, p, c) => true);
+
+  submit() {
+    print("xyx");
+  }
+
+  @override
+  void dispose() {
+    _accountController?.close();
+    _passwordController?.close();
+    _captchaController?.close();
+  }
+}
+
+abstract class BaseBloc {
+  void dispose();
+}
---
+++ lib/utils/validators_login.dart
@@ -0,0 +1,30 @@
+import 'dart:async';
+
+mixin LoginValidators {
+  var accountValidator =
+      StreamTransformer<String, String>.fromHandlers(handleData: (account, sink) {
+    if (account.length > 0) {
+      sink.add(account);
+    } else {
+      sink.addError("Account can't be null.");
+    }
+  });
+
+  var passwordValidator =
+      StreamTransformer<String, String>.fromHandlers(handleData: (password, sink) {
+    if (password.length > 0) {
+      sink.add(password);
+    } else {
+      sink.addError("Password can't be null.");
+    }
+  });
+
+  var captchaValidator =
+      StreamTransformer<String, String>.fromHandlers(handleData: (captcha, sink) {
+    if (captcha.length > 4) {
+      sink.add(captcha);
+    } else {
+      sink.addError("Captcha length should be greater than 4 chars.");
+    }
+  });
+}
