6e580c42caa846f060221239d50f772ba9ea3420
wml <mxl1989@gmail.com>
2019-01-06 22:47:33 +0800

    prepare login

--- lib/page_login.dart
+++ lib/page_login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/bloc/bloc_login.dart';
 import 'package:flutter_app/model/web/login_form_data.dart';
 import 'package:flutter_app/network/api_web.dart';
 
@@ -31,38 +32,53 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
+    final bloc = BlocLogin();
     final logo = Image.asset("assets/images/logo_v2lf.png");
 
-    final userName = TextFormField(
-      autofocus: false,
-      decoration: InputDecoration(
-          hintText: 'account',
-          contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-          border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+    final userName = StreamBuilder<String>(
+      stream: bloc.account,
+      builder: (context, snapshot) => TextField(
+            onChanged: bloc.accountChanged,
+            autofocus: false,
+            decoration: InputDecoration(
+                errorText: snapshot.error,
+                hintText: 'account',
+                contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+                border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+          ),
     );
 
-    final password = TextFormField(
-      autofocus: false,
-      obscureText: true,
-      decoration: InputDecoration(
-          hintText: 'password',
-          contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-          border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+    final password = StreamBuilder<String>(
+      stream: bloc.password,
+      builder: (context, snapshot) => TextField(
+            onChanged: bloc.passwordChanged,
+            autofocus: false,
+            obscureText: true,
+            decoration: InputDecoration(
+                errorText: snapshot.error,
+                hintText: 'password',
+                contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+                border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+          ),
     );
 
     final captcha = Row(
       children: <Widget>[
         Flexible(
-            child: TextFormField(
-          autofocus: false,
-          decoration: InputDecoration(
-              hintText: 'captcha',
-              contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-              border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
-        )),
+            child: StreamBuilder<String>(
+                stream: bloc.captcha,
+                builder: (context, snapshot) => TextField(
+                      onChanged: bloc.captchaChanged,
+                      autofocus: false,
+                      decoration: InputDecoration(
+                          errorText: snapshot.error,
+                          hintText: 'captcha',
+                          contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+                          border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+                    ))),
         Padding(
             padding: const EdgeInsets.only(left: 8.0),
-            child: loginFormData.bytes.length > 0
+            child: loginFormData != null && loginFormData.bytes.length > 0
                 ? GestureDetector(
                     child: Image.memory(
                       loginFormData.bytes,
@@ -81,21 +97,28 @@ class _LoginPageState extends State<LoginPage> {
       ],
     );
 
-    final loginButton = Material(
-      clipBehavior: Clip.antiAlias,
-      borderRadius: BorderRadius.circular(30.0),
-      shadowColor: Colors.blueGrey.shade100,
-      elevation: 5.0,
-      child: MaterialButton(
-        minWidth: double.infinity,
-        height: 48.0,
-        onPressed: () {
-          // 登录
-          // todo 非空检查-登录判断结果
-        },
-        color: Colors.blueGrey,
-        child: Text('Log In', style: TextStyle(color: Colors.white)),
-      ),
+    final loginButton = StreamBuilder<bool>(
+      stream: bloc.submitCheck,
+      builder: (context, snapshot) => Material(
+            clipBehavior: Clip.antiAlias,
+            borderRadius: BorderRadius.circular(30.0),
+            shadowColor: Colors.blueGrey.shade100,
+            elevation: 5.0,
+            child: MaterialButton(
+              color: Colors.blueGrey,
+              minWidth: double.infinity,
+              height: 48.0,
+              disabledColor: Colors.grey,
+              onPressed: snapshot.hasData
+                  ? () {
+                      // 登录
+                      // todo 非空检查-登录判断结果
+                      print("xxx");
+                    }
+                  : null,
+              child: Text('Log In', style: TextStyle(color: Colors.white)),
+            ),
+          ),
     );
 
     final forgotLabel = FlatButton(
