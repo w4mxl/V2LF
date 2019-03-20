127fb39b00c794baf6b45cdd5121f4a5dfe569f8
wml-office <mxl1989@gmail.com>
2019-03-20 19:07:22 +0800

    - 新增：搜索页面初步实现

--- lib/components/search_delegate.dart
+++ lib/components/search_delegate.dart
@@ -1,27 +1,22 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_app/model/sov2ex.dart';
 import 'package:flutter_app/model/web/node.dart';
+import 'package:flutter_html/flutter_html.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
-/**
- * @author: wml
- * @date  : 2019/3/19 10:10 PM
- * @email : mxl1989@gmail.com
- * @desc  : SearchDelegate
- */
-
+/// @author: wml
+/// @date  : 2019/3/19 10:10 PM
+/// @email : mxl1989@gmail.com
+/// @desc  : SearchDelegate
 
 List<NodeItem> allNodes = <NodeItem>[];
 
 class MySearchDelegate extends SearchDelegate<String> {
-  final List<NodeItem> meLikeNodes = <NodeItem>[
-    NodeItem('share', '分享发现'),
-    NodeItem('programmer', '程序员'),
-    NodeItem('kindle', 'Kindle'),
-    NodeItem('iphone', 'iPhone'),
-    NodeItem('ipad', 'iPad'),
-    NodeItem('invest', '投资'),
-    NodeItem('jobs', '酷工作'),
-    NodeItem('shanghai', '上海'),
-  ];
+  final List<String> _history = ['v2er', 'AirPods'];
 
   @override
   List<Widget> buildActions(BuildContext context) {
@@ -30,6 +25,7 @@ class MySearchDelegate extends SearchDelegate<String> {
         icon: Icon(Icons.close),
         onPressed: () {
           query = "";
+          showSuggestions(context);
         },
       )
     ];
@@ -47,29 +43,118 @@ class MySearchDelegate extends SearchDelegate<String> {
 
   @override
   Widget buildResults(BuildContext context) {
-    return Center(child: Text('┐(´-｀)┌'));
+//    return Center(child: Text('┐(´-｀)┌'));
+    return buildSearchFutureBuilder(query.trim());
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    final suggestionNodes = query.isEmpty ? meLikeNodes : allNodes.where((p) => p.nodeName.startsWith(query)).toList();
+//    final suggestionNodes = query.isEmpty ? meLikeNodes : allNodes.where((p) => p.nodeName.startsWith(query)).toList();
 
     return ListView.builder(
       itemBuilder: (context, index) => ListTile(
-        leading: Icon(Icons.navigate_next),
-        title: RichText(
-            text: TextSpan(
-                text: suggestionNodes[index].nodeName.substring(0, query.length),
-                style: TextStyle(color: Colors.black87, fontWeight: FontWeight.bold),
-                children: [
-                  TextSpan(
-                      text: suggestionNodes[index].nodeName.substring(query.length),
-                      style: DefaultTextStyle.of(context).style)
-                ])),
-        onTap: null
+            leading: Icon(Icons.history),
+            title: Text(_history[index]),
+            onTap: () {},
 //            Navigator.push(context, MaterialPageRoute(builder: (context) => new NodeTopics(suggestionNodes[index]))),
+          ),
+      itemCount: _history.length,
+    );
+  }
+
+  FutureBuilder<Sov2ex> buildSearchFutureBuilder(String q) {
+    return new FutureBuilder<Sov2ex>(
+      future: getSov2exData(q),
+      builder: (context, AsyncSnapshot<Sov2ex> async) {
+        if (async.connectionState == ConnectionState.active || async.connectionState == ConnectionState.waiting) {
+          return new Center(
+            child: new CircularProgressIndicator(),
+          );
+        }
+
+        if (async.connectionState == ConnectionState.done) {
+          if (async.hasError) {
+            return new Center(
+              child: new Text('${async.error}'),
+            );
+          } else if (async.hasData) {
+            Sov2ex sov2ex = async.data;
+            return Sov2exResultListView(sov2ex.hits);
+          }
+        }
+      },
+    );
+  }
+
+  Future<Sov2ex> getSov2exData(String q) async {
+    var dio = Dio();
+    try {
+      var response = await dio.get('https://www.sov2ex.com/api/search?size=50&q=' + q);
+      print(response.data);
+      return Sov2ex.fromMap(response.data);
+    } on DioError catch (e) {
+      Fluttertoast.showToast(msg: '搜索失败');
+      print(e.response.data);
+      print(e.response.headers);
+      print(e.response.request);
+      return null;
+    }
+  }
+}
+
+class Sov2exResultListView extends StatelessWidget {
+  final List<HitsListBean> hits;
+
+  Sov2exResultListView(this.hits);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: hits.length,
+        itemBuilder: (context, index) {
+          return Sov2exResultItem(hits[index]);
+        });
+  }
+}
+
+class Sov2exResultItem extends StatelessWidget {
+  final HitsListBean hitsListBean;
+
+  Sov2exResultItem(this.hitsListBean);
+
+  @override
+  Widget build(BuildContext context) {
+    String content = hitsListBean.highlight.content != null
+        ? hitsListBean.highlight.content[0].replaceAll('<em>', '<mark>').replaceAll('<\/em>', '<\/mark>')
+        : (hitsListBean.highlight.postscript_list != null
+            ? hitsListBean.highlight.postscript_list[0].replaceAll('<em>', '<mark>').replaceAll('<\/em>', '<\/mark>')
+            : (hitsListBean.highlight.reply_list != null
+                ? hitsListBean.highlight.reply_list[0].replaceAll('<em>', '<mark>').replaceAll('<\/em>', '<\/mark>')
+                : hitsListBean.source.content));
+
+    return Container(
+      padding: EdgeInsets.all(10.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Html(
+            data: hitsListBean.highlight.title != null
+                ? hitsListBean.highlight.title[0].replaceAll('<em>', '<mark>').replaceAll('<\/em>', '<\/mark>')
+                : hitsListBean.source.title,
+            defaultTextStyle: TextStyle(color: Colors.black87, fontSize: 18.0),
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          Html(
+            data: content,
+            defaultTextStyle: TextStyle(color: Colors.black54, fontSize: 15.0),
+          ),
+          Divider(
+            height: 6.0,
+          )
+        ],
       ),
-      itemCount: suggestionNodes.length,
     );
   }
 }
---
+++ lib/model/sov2ex.dart
@@ -0,0 +1,131 @@
+/// @author: wml
+/// @date  : 2019/3/20 12:39
+/// @email : mxl1989@gmail.com
+/// @desc  : 借助 sov2ex 搜索
+
+class Sov2ex {
+  bool timed_out;
+  int took;
+  int total;
+  List<HitsListBean> hits;
+
+  static Sov2ex fromMap(Map<String, dynamic> map) {
+    Sov2ex sov2ex = new Sov2ex();
+    sov2ex.timed_out = map['timed_out'];
+    sov2ex.took = map['took'];
+    sov2ex.total = map['total'];
+    sov2ex.hits = HitsListBean.fromMapList(map['hits']);
+    return sov2ex;
+  }
+
+  static List<Sov2ex> fromMapList(dynamic mapList) {
+    List<Sov2ex> list = new List(mapList.length);
+    for (int i = 0; i < mapList.length; i++) {
+      list[i] = fromMap(mapList[i]);
+    }
+    return list;
+  }
+}
+
+class HitsListBean {
+  String index;
+  String type;
+  String id;
+  double score;
+  SourceBean source;
+  HighlightBean highlight;
+
+  static HitsListBean fromMap(Map<String, dynamic> map) {
+    HitsListBean hitsListBean = new HitsListBean();
+    hitsListBean.index = map['_index'];
+    hitsListBean.type = map['_type'];
+    hitsListBean.id = map['_id'];
+    hitsListBean.score = map['_score'];
+    hitsListBean.source = SourceBean.fromMap(map['_source']);
+    hitsListBean.highlight = HighlightBean.fromMap(map['highlight']);
+    return hitsListBean;
+  }
+
+  static List<HitsListBean> fromMapList(dynamic mapList) {
+    List<HitsListBean> list = new List(mapList.length);
+    for (int i = 0; i < mapList.length; i++) {
+      list[i] = fromMap(mapList[i]);
+    }
+    return list;
+  }
+}
+
+class SourceBean {
+  String created;
+  String member;
+  String title;
+  String content;
+  int node;
+  int replies;
+  int id;
+
+  static SourceBean fromMap(Map<String, dynamic> map) {
+    SourceBean _sourceBean = new SourceBean();
+    _sourceBean.created = map['created'];
+    _sourceBean.member = map['member'];
+    _sourceBean.title = map['title'];
+    _sourceBean.content = map['content'];
+    _sourceBean.node = map['node'];
+    _sourceBean.replies = map['replies'];
+    _sourceBean.id = map['id'];
+    return _sourceBean;
+  }
+
+  static List<SourceBean> fromMapList(dynamic mapList) {
+    List<SourceBean> list = new List(mapList.length);
+    for (int i = 0; i < mapList.length; i++) {
+      list[i] = fromMap(mapList[i]);
+    }
+    return list;
+  }
+}
+
+class HighlightBean {
+  List<String> content;
+  List<String> postscript_list;
+  List<String> reply_list;
+  List<String> title;
+
+  static HighlightBean fromMap(Map<String, dynamic> map) {
+    HighlightBean highlightBean = new HighlightBean();
+
+    if (map['content'] != null) {
+      List<dynamic> dynamicList0 = map['content'];
+      highlightBean.content = new List();
+      highlightBean.content.addAll(dynamicList0.map((o) => o.toString()));
+    }
+
+    if (map['postscript_list.content'] != null) {
+      List<dynamic> dynamicList1 = map['postscript_list.content'];
+      highlightBean.postscript_list = new List();
+      highlightBean.postscript_list.addAll(dynamicList1.map((o) => o.toString()));
+    }
+
+    if (map['reply_list.content'] != null) {
+      List<dynamic> dynamicList2 = map['reply_list.content'];
+      highlightBean.reply_list = new List();
+      highlightBean.reply_list.addAll(dynamicList2.map((o) => o.toString()));
+    }
+
+    if (map['title'] != null) {
+      List<dynamic> dynamicList3 = map['title'];
+      highlightBean.title = new List();
+      highlightBean.title.addAll(dynamicList3.map((o) => o.toString()));
+    }
+
+    return highlightBean;
+  }
+
+  static List<HighlightBean> fromMapList(dynamic mapList) {
+    List<HighlightBean> list = new List(mapList.length);
+    for (int i = 0; i < mapList.length; i++) {
+      list[i] = fromMap(mapList[i]);
+    }
+    return list;
+  }
+}
