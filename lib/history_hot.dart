ee81d209f88d183065ae0d69dcd3567fa08214cd
wml-office <mxl1989@gmail.com>
2019-03-25 14:17:29 +0800

    - 完善：往期热点支持跳转到app内详情页

--- lib/history_hot.dart
+++ lib/history_hot.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_app/page_topic_detail.dart';
 import 'package:flutter_app/utils/bubble_tab_indicator.dart';
 import 'package:http/http.dart';
 import 'package:webfeed/webfeed.dart';
@@ -39,7 +40,7 @@ class _HistoryHotTopicsState extends State<HistoryHotTopics> with AutomaticKeepA
           appBar: AppBar(
             title: TabBar(
                 isScrollable: true,
-                unselectedLabelColor: Colors.grey,
+//                unselectedLabelColor: Colors.grey,
                 labelColor: Colors.white,
                 indicatorSize: TabBarIndicatorSize.tab,
                 indicator: new BubbleTabIndicator(
@@ -51,10 +52,6 @@ class _HistoryHotTopicsState extends State<HistoryHotTopics> with AutomaticKeepA
           ),
           body: buildFutureBuilder(),
         ));
-//    return Scaffold(
-//      appBar: AppBar(),
-//      body: buildFutureBuilder(),
-//    );
   }
 
   FutureBuilder<AtomFeed> buildFutureBuilder() {
@@ -62,64 +59,19 @@ class _HistoryHotTopicsState extends State<HistoryHotTopics> with AutomaticKeepA
         future: _future,
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            print(snapshot.data.updated.split('T')[0]);
-            print(
-                snapshot.data.items.lastIndexWhere((item) => item.updated.startsWith(snapshot.data.updated.split('T')[0])));
+            var splitDate = snapshot.data.updated.split('T')[0];
 
-            List<AtomItem> yesterdayList = snapshot.data.items.sublist(
-                0,
-                snapshot.data.items.lastIndexWhere((item) => item.updated.startsWith(snapshot.data.updated.split('T')[0])) +
-                    1);
-            List<AtomItem> anotherList = snapshot.data.items.sublist(
-                snapshot.data.items.lastIndexWhere((item) => item.updated.startsWith(snapshot.data.updated.split('T')[0])) +
-                    1);
+            print(snapshot.data.items.lastIndexWhere((item) => item.updated.startsWith(splitDate)));
+            var lastIndexWhere = snapshot.data.items.lastIndexWhere((item) => item.updated.startsWith(splitDate));
+
+            List<AtomItem> yesterdayList = snapshot.data.items.sublist(0, lastIndexWhere + 1);
+            List<AtomItem> beforeYesterdayList = snapshot.data.items.sublist(lastIndexWhere + 1);
 
             return TabBarView(
               children: <Widget>[
-                Scrollbar(
-                  child: ListView(
-                    children: ListTile.divideTiles(
-                            context: context,
-                            tiles: yesterdayList.map((AtomItem item) {
-                              return ListTile(
-                                contentPadding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 4.0),
-                                title: Text(item.title.split(']')[1]),
-                                subtitle:
-                                    Text('${item.title.split(']')[0].replaceFirst('[', '')} · ${item.authors[0].name}'),
-                              );
-                            }).toList())
-                        .toList(),
-                  ),
-                ),
-                Scrollbar(
-                  child: ListView(
-                    children: ListTile.divideTiles(
-                            context: context,
-                            tiles: anotherList.map((AtomItem item) {
-                              return ListTile(
-                                contentPadding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 4.0),
-                                title: Text(item.title.split(']')[1]),
-                                subtitle: Text('${item.title.split(']')[0].replaceFirst('[', '')} · ${item.authors[0].name}'),
-                              );
-                            }).toList())
-                        .toList(),
-                  ),
-                ),
+                new TabBarViewChild(dayTopicList: yesterdayList),
+                new TabBarViewChild(dayTopicList: beforeYesterdayList)
               ],
-//              child: Scrollbar(
-//                child: ListView(
-//                  children: ListTile.divideTiles(
-//                          context: context,
-//                          tiles: snapshot.data.items.map((AtomItem item) {
-//                            return ListTile(
-//                              contentPadding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 4.0),
-//                              title: Text(item.title.split(']')[1]),
-//                              subtitle: Text('${item.title.split(']')[0]}] · ${item.authors[0].name}'),
-//                            );
-//                          }).toList())
-//                      .toList(),
-//                ),
-//              ),
             );
           } else if (snapshot.hasError) {
             return Center(
@@ -148,4 +100,35 @@ class _HistoryHotTopicsState extends State<HistoryHotTopics> with AutomaticKeepA
 
   @override
   bool get wantKeepAlive => true;
+}
+
+class TabBarViewChild extends StatelessWidget {
+  const TabBarViewChild({
+    Key key,
+    @required this.dayTopicList,
+  }) : super(key: key);
+
+  final List<AtomItem> dayTopicList;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scrollbar(
+      child: ListView(
+        children: ListTile.divideTiles(
+                context: context,
+                tiles: dayTopicList.map((AtomItem item) {
+                  return ListTile(
+                    contentPadding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 4.0),
+                    title: Text(item.title.split(']')[1]),
+                    subtitle: Text('${item.title.split(']')[0].replaceFirst('[', '')} · ${item.authors[0].name}'),
+                    onTap: () => Navigator.push(
+                          context,
+                          new MaterialPageRoute(builder: (context) => new TopicDetails(item.links[0].href.split('/t/')[1])),
+                        ),
+                  );
+                }).toList())
+            .toList(),
+      ),
+    );
+  }
 }
