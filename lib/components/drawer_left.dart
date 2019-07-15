8642fe2c05023e3fa81d46e18c9979c33310ab92
wml-office <mxl1989@gmail.com>
2019-07-15 14:15:24 +0800

    chore：left drawer optimize

--- lib/components/drawer_left.dart
+++ lib/components/drawer_left.dart
@@ -197,7 +197,7 @@ class _DrawerLeftState extends State<DrawerLeft> {
                         borderRadius: BorderRadius.all(Radius.circular(36.0)), // currentAccountPicture 宽高是72
                         border: Border.all(
                           color: Colors.white,
-                          width: 1.5,
+                          width: 1.8,
                         )),
                   ),
                 ),
@@ -270,6 +270,7 @@ class _DrawerLeftState extends State<DrawerLeft> {
                 height: 0,
               ),
               new ListTile(
+                enabled: userName.isNotEmpty,
                 leading: new Icon(Icons.add),
                 title: new Text(S.of(context).create),
                 onTap: () {
