b2c6daeede1bf2a9a7b8eb8ff260a32fe1dd5200
wml-office <mxl1989@gmail.com>
2020-03-13 22:31:16 +0800

    2020.3+37

--- lib/components/drawer_left.dart
+++ lib/components/drawer_left.dart
@@ -465,7 +465,7 @@ class _DrawerLeftState extends State<DrawerLeft> {
                 icon: new Icon(Icons.info),
                 child: new Text(S.of(context).about),
                 applicationName: "V2LF",
-                applicationVersion: "v2020.2",
+                applicationVersion: "v2020.3",
                 applicationLegalese: '© 2020 Wml',
                 applicationIcon: new Image.asset(
                   "assets/images/icon/ic_launcher.png",
