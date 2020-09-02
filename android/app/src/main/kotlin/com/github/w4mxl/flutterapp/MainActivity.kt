e9e68f09d935857b67fd44d5ed8ec701d7920786
wml-office <mxl1989@gmail.com>
2020-09-02 09:32:24 +0800

    fix: update to android embedding v2

--- android/app/src/main/kotlin/com/github/w4mxl/flutterapp/MainActivity.kt
+++ android/app/src/main/kotlin/com/github/w4mxl/flutterapp/MainActivity.kt
@@ -1,13 +1,7 @@
 package com.github.w4mxl.flutterapp
 
-import android.os.Bundle
-
-import io.flutter.app.FlutterActivity
-import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.embedding.android.FlutterActivity;
 
 class MainActivity(): FlutterActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    GeneratedPluginRegistrant.registerWith(this)
-  }
+
 }
